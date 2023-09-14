dc4afb47efaf49acd648a7d260f450001b6c3439
nayconsantos <josenaycon1@gmail.com>
2023-09-14 12:28:46 -0300

    commit

---
+++ mundo1/exercicios/ex004.py
@@ -0,0 +1,5 @@
+frase = input('Digite algo: ')
+print(f'É numerico ? {frase.isnumeric()}')
+print(f'É maiusculo ? {frase.isupper()}')
+print(f'É minusculo ? {frase.islower()}')
+print(f'É numerico e alfabetico ? {frase.isalnum()}')
